79dbc680d2ffb6b27eabf96537d83fce52cdf585
caloloc2 <calolomino@gmail.com>
2022-07-24 20:55:50 -0500

    act

--- php/ubicacion.php
+++ php/ubicacion.php
@@ -12,8 +12,8 @@ $database = $factory->createDatabase();
 
 if (((isset($_GET['lng'])) && (!empty($_GET['lng']))) && ((isset($_GET['lat'])) && (!empty($_GET['lat'])))){
     
-    // $database->getReference('locacion/longitud')->set($_GET['lng']);
-    // $database->getReference('locacion/latitud')->set($_GET['lat']);
+    $database->getReference('locacion/longitud')->set($_GET['lng']);
+    $database->getReference('locacion/latitud')->set($_GET['lat']);
 
     echo "ok";
 }
