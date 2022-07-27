8faddf54049621efa4b08d0b308ee93451c59cb2
caloloc2 <calolomino@gmail.com>
2022-07-26 22:15:03 -0500

    act

--- libs/firebase.py
+++ libs/firebase.py
@@ -6,8 +6,9 @@ class Firebase:
 
     def __init__(self):
         self.server = ServerBridge()
-        self.bloqueo = GPIORasp(16)
-        self.activacion = GPIORasp(21)
+        self.bloqueo = GPIORasp(21)
+        self.activacion = GPIORasp(16)
+        self.desactivacion = GPIORasp(20)
 
     def lectura(self):
         while(True):
@@ -15,6 +16,10 @@ class Firebase:
             print(data)
             estadoLed = data['estados']['bloqueo']
             self.bloqueo.accion(estadoLed)
+
             estadoLed = data['estados']['activacion']
             self.activacion.accion(estadoLed)
+
+            estadoLed = data['estados']['desactivacion']
+            self.desactivacion.accion(estadoLed)
             time.sleep(2)
