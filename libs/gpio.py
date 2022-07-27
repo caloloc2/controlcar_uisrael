00975cf545bb332d4b39403ad1ef753af4f284b1
caloloc2 <calolomino@gmail.com>
2022-07-26 22:33:59 -0500

    act

--- libs/gpio.py
+++ libs/gpio.py
@@ -10,7 +10,7 @@ class GPIORasp:
         if (mode == 1):
             GPIO.setup(self.pin, GPIO.OUT)
         elif (mode == 0):
-            GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setwarnings(False)
  
     def accion(self, state = False):
