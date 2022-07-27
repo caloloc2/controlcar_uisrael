7eb1708f4249339e50b49d485c815a65df3f82ea
caloloc2 <calolomino@gmail.com>
2022-07-26 23:32:12 -0500

    act

--- libs/nfc.py
+++ libs/nfc.py
@@ -4,6 +4,7 @@ from py532lib.i2c import *
 from py532lib.frame import *
 from py532lib.constants import *
 from libs.gpio import GPIORasp
+from libs.httpRequest import ServerBridge
 
 class NFCLector:
 
@@ -11,6 +12,7 @@ class NFCLector:
         self.pn532 = Pn532_i2c()
         self.pn532.SAMconfigure()
         self.naranja = GPIORasp(26)
+        self.server = ServerBridge()
 
     def __convert__(self, byte):
         return int.from_bytes(byte, "big")
@@ -23,5 +25,9 @@ class NFCLector:
 
             if (codigo == 90674177285458845226483794):
                 self.naranja.accion(True)
+                params = {'valor': 0}
+                self.server.get('inicio.php', params)
                 time.sleep(5)
                 self.naranja.accion(False)
+                params = {'valor': 0}
+                self.server.get('inicio.php', params)
---
+++ php/inicio.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+
+$factory = (new Factory)->withServiceAccount('llave.json')->withDatabaseUri('https://carrouisrael-default-rtdb.firebaseio.com/');
+
+
+$database = $factory->createDatabase();
+
+$respuesta['estado'] = false;
+
+$database->getReference('inicio')->set($_GET['valor']);
+$respuesta['estado'] = true;
+
+echo json_encode($respuesta);
