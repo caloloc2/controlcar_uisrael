a5d6519f1a4710e37d34516f73dc368f8003ff2b
caloloc2 <calolomino@gmail.com>
2022-08-06 23:01:47 -0500

    ac

--- web/libs/camara.py
+++ web/libs/camara.py
@@ -19,6 +19,8 @@ class Camara:
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         print("[Listo para reconocimiento]")
 
+        self.estado = 0
+
         f = Thread(target = self.crontab)
         f.daemon = True
         f.start()
@@ -29,32 +31,32 @@ class Camara:
             print(data)
             time.sleep(3)
 
-
     def reconocimiento(self):
         while True:
             success, image = self.video.read()
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            auxFrame = gray.copy()
-            faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
 
-            try:
-                for (x,y,w,h) in faces:
-                    rostro = auxFrame[y:y+h,x:x+w]
-                    rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-                    result = self.face_recognizer.predict(rostro)
+            if (self.estado == 1):
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                auxFrame = gray.copy()
+                faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+                try:
+                    for (x,y,w,h) in faces:
+                        rostro = auxFrame[y:y+h,x:x+w]
+                        rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+                        result = self.face_recognizer.predict(rostro)
 
-                    cv2.putText(image,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-                
-                    if (len(result) > 0):
-                        # LBPHFace
-                        if result[1] < 70:
-                            cv2.putText(image,'{}'.format(self.imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-                            cv2.rectangle(image, (x,y),(x+w,y+h),(0,255,0),2)
-                        else:
-                            cv2.putText(image,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-                            cv2.rectangle(image, (x,y),(x+w,y+h),(0,0,255),2)
-            except:
-                print("Error")
+                        cv2.putText(image,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+                    
+                        if (len(result) > 0):
+                            # LBPHFace
+                            if result[1] < 70:
+                                cv2.putText(image,'{}'.format(self.imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+                                cv2.rectangle(image, (x,y),(x+w,y+h),(0,255,0),2)
+                            else:
+                                cv2.putText(image,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+                                cv2.rectangle(image, (x,y),(x+w,y+h),(0,0,255),2)
+                except:
+                    print("Error")
 
             return cv2.imencode('.jpg', image)
     
