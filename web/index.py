c78576a9a430d7a55dc96614c66ba95f0fa32c2c
caloloc2 <calolomino@gmail.com>
2022-08-06 22:04:53 -0500

    ac

--- web/index.py
+++ web/index.py
@@ -1,21 +1,9 @@
 from flask import Flask, Response, render_template
-import cv2
-import os
+from libs.camara import Camara
 
-dataPath = 'Data' #Cambia a la ruta donde hayas almacenado Data
-imagePaths = os.listdir(dataPath)
-print('imagePaths=',imagePaths)
-
-face_recognizer = cv2.face.EigenFaceRecognizer_create()
-face_recognizer = cv2.face.FisherFaceRecognizer_create()
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-face_recognizer.read('modeloLBPHFace.xml')
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+camara = Camara()
 
 app = Flask(__name__)
-video = cv2.VideoCapture(0)
 
 @app.route('/')
 def index():
@@ -23,27 +11,7 @@ def index():
 
 def gen(video):
     while True:
-        success, image = video.read()
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        auxFrame = gray.copy()
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-        for (x,y,w,h) in faces:
-            rostro = auxFrame[y:y+h,x:x+w]
-            rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-            result = face_recognizer.predict(rostro)
-
-            cv2.putText(image,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-        
-            # LBPHFace
-            if result[1] < 70:
-                cv2.putText(image,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-                cv2.rectangle(image, (x,y),(x+w,y+h),(0,255,0),2)
-            else:
-                cv2.putText(image,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-                cv2.rectangle(image, (x,y),(x+w,y+h),(0,0,255),2)
-
-        ret, jpeg = cv2.imencode('.jpg', image)
+        ret, jpeg = camara.reconocimiento()
         frame = jpeg.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
---
+++ web/libs/camara.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+
+class Camara:
+
+    def __init__(self):
+        self.dataPath = 'Data'
+        self.imagePaths = os.listdir(self.dataPath)
+        print("[Iniciando camara...]")
+        self.video = cv2.VideoCapture(0)
+        print("[Iniciando reconocimiento...]")
+        self.face_recognizer = cv2.face.EigenFaceRecognizer_create()
+        self.face_recognizer = cv2.face.FisherFaceRecognizer_create()
+        self.face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+        self.face_recognizer.read('modeloLBPHFace.xml')
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        print("[Listo para reconocimiento]")
+
+    def reconocimiento(self):
+        while True:
+            success, image = self.video.read()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            auxFrame = gray.copy()
+            faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            for (x,y,w,h) in faces:
+                rostro = auxFrame[y:y+h,x:x+w]
+                rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+                result = self.face_recognizer.predict(rostro)
+
+                cv2.putText(image,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+            
+                # LBPHFace
+                if result[1] < 70:
+                    cv2.putText(image,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+                    cv2.rectangle(image, (x,y),(x+w,y+h),(0,255,0),2)
+                else:
+                    cv2.putText(image,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+                    cv2.rectangle(image, (x,y),(x+w,y+h),(0,0,255),2)
+
+            return cv2.imencode('.jpg', image)
