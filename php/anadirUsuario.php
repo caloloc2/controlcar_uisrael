1638e9e4796ea5044bd35686dd64213c3aede1dd
caloloc2 <calolomino@gmail.com>
2022-08-07 00:47:14 -0500

    ac

--- php/anadirUsuario.php
+++ php/anadirUsuario.php
@@ -9,10 +9,14 @@ $factory = (new Factory)->withServiceAccount('llave.json')->withDatabaseUri('htt
 
 $database = $factory->createDatabase();
 
+$nombreUsuario = "";
 $nuevo = $database->getReference('nuevo')->getSnapshot()->getValue();
+$nombreUsuario = $database->getReference('nombreUsuario')->getSnapshot()->getValue();
+
 $respuesta['estado'] = false;
 if ($nuevo == 1){
     $respuesta['estado'] = true;
+    $respuesta['nombre'] = $nombreUsuario;
 }
 
 echo json_encode($respuesta);
