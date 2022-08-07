d7327faea866b70c88b0e0c74629cdf4e3abe8a7
caloloc2 <calolomino@gmail.com>
2022-08-06 21:45:57 -0500

    act

---
+++ web/index.py
@@ -0,0 +1,46 @@
+from flask import Flask, Response, render_template
+import cv2
+import os
+
+dataPath = 'Data' #Cambia a la ruta donde hayas almacenado Data
+imagePaths = os.listdir(dataPath)
+# print('imagePaths=',imagePaths)
+
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+face_recognizer.read('modeloLBPHFace.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+app = Flask(__name__)
+video = cv2.VideoCapture(0)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(video):
+    while True:
+        success, image = video.read()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image,  (x,y), (x+w, y+h), (255,0,0), 2)
+        
+        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    global video
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2204, threaded=True)
