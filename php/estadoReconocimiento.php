5d905e7720678fa65a991bbde0769b97437355fb
caloloc2 <calolomino@gmail.com>
2022-08-07 19:58:22 -0500

    ac

---
+++ php/estadoReconocimiento.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+
+$factory = (new Factory)->withServiceAccount('llave.json')->withDatabaseUri('https://carrouisrael-default-rtdb.firebaseio.com/');
+
+
+$database = $factory->createDatabase();
+
+$respuesta['estado'] = false;
+
+if ((isset($_GET['info'])) && (!empty($_GET['info']))){
+    $database->getReference('informacion')->set($_GET['info']);
+    $respuesta['estado'] = true;
+}
+
+echo json_encode($respuesta);
